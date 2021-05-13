42326a183d96f2f57769dda1960d9df9fd784cd7
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-05-13 20:21:23 +0300

    Update timer.js

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,107 +1,39 @@
-const refs = {
-    daysRef: document.querySelector('[data-value="days"]'),
-    hoursRef: document.querySelector('[data-value="hours"]'),
-    minsRef: document.querySelector('[data-value="mins"]'),
-    secsRef: document.querySelector('[data-value="secs"]'),
+//Два Taймера обратного отсчет
+
+class CountdownTimer  {   //занимается только подчетом. не обновляет интерфес!
+  constructor({ selector, targetDate}) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = targetDate;
   };
-  
-  class CountdownTimer  {         //занимается только подчетом. не обновляет интерфес!
-    constructor({ selector, targetDate, onTimer } = {}) {
-      this.intervalId = null;
-      this.selector = selector;
-      this.targetDate = targetDate;
-      this.onTimer = onTimer;
-      this.init();
-    };
-    
-    init() {    
-      const timeComponents =  this.getTimeComponents(0);
-      this.onTimer(timeComponents);
-    }
-    start() {                      
-      this.intervalId = setInterval(() => {           //this. потому-что это уже свойство объекта
-      // console.log('Каждую секунду вызываем функцию');
-    
-      const currentTime = Date.now();              //это время каждый раз новое.
+
+  start() { 
+    console.log(this.selector.querySelector('[data-value="days"]'));                   
+  setInterval(() => {     
+  const time = this.targetDate - Date.now(); 
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 
+    60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+  this.changeTime(days, hours, mins, secs);
      
-      const differenceTime = this.targetDate - currentTime;
-      const time =  this.getTimeComponents(differenceTime);
-      console.log(time);
-
-      this.onTimer(time);      
-      }, 1000);
-        }
-
-  //  // 2 вар просто счетчик 
-  //       start() {                      
-  //         const startTime = Date.now();  // это стартующее,текущее время. Оно всегда одинаковое. момент создания.
-          
-  //         this.intervalId = setInterval(() => {           
-    
-  //         const currentTime = Date.now();          
-  //         // console.log('startTime', startTime);
-  //         //  console.log('currentTime', currentTime);
-  //         const differenceTime = currentTime - startTime; 
-  //         const time =  this.getTimeComponents(differenceTime);
-  //         console.log(time);
-    
-  //         this.onTimer(time);     
-  //         }, 1000);
-  //           }
-
-      getTimeComponents(time) {
-      /*
-     * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-     * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-     */
-    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-
-    /*
-     * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-     * остатка % и делим его на количество миллисекунд в одном часе
-     * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-     */
-    const hours = this.pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    );
-
-    /*
-     * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-     * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-     */
-    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
-    /*
-     * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-     * миллисекунд в одной секунде (1000)
-     */
-    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-
-    return { days, hours, mins, secs };
+  }, 1000);
+ }
+  changeTime(days, hours, mins, secs) {
+    this.selector.querySelector('[data-value="days"]').textContent = days;
+    this.selector.querySelector('[data-value="hours"]').textContent = hours;
+    this.selector.querySelector('[data-value="mins"]').textContent = mins;
+    this.selector.querySelector('[data-value="secs"]').textContent = secs;
   }
+}   
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2021'),
+}).start();
 
-  // eslint-disable-next-line class-methods-use-this
-  pad(value) {
-    return String(value).padStart(2, '0');
-  }
-}
-/*
- * - Принимает время в миллисекундах
- * - Высчитывает сколько в них вмещается часов/минут/секунд
- * - Рисует интерфейс
- */
-function updateClockface({days, hours, mins, secs }) {  //метод добав. интерфейса
-  refs.daysRef.textContent = `${days}`;
-  refs.hoursRef.textContent = `${hours}`;
-  refs.minsRef.textContent = `${mins}`;
-  refs.secsRef.textContent = `${secs}`;
-}
-  
- const timer = new CountdownTimer({    //это объект настроек
-    selector: '#timer-1',
-    targetDate: new Date('May 12, 2021 23:59:00'),
-    onTimer: updateClockface,         //метод добав. интерфейса перед. как свойство таймера, class CountdownTimer.
-  });
+new CountdownTimer({
+  selector: '#timer-2',
+  targetDate: new Date('Jul 17, 2022'),
+}).start();
 
-timer.start(); //вызываем, что-бы начал считать. Когда я вызываю метод start, я хочу сохраниить текущее время.
   
